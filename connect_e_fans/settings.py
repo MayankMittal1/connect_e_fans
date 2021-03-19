968f7233d5f1b22617d10978391a423aa0b6bc22
Mayank <mmittal123456@gmail.com>
2021-03-20 01:25:01 +0530

    final

--- connect_e_fans/settings.py
+++ connect_e_fans/settings.py
@@ -144,7 +144,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'connect.e.fans@gmail.com'
+EMAIL_HOST_USER = 'connect.e.fans721@gmail.com'
 EMAIL_HOST_PASSWORD = 'connect@21052002'
 # Activate Django-Heroku.
 
